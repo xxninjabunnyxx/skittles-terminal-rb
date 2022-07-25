e8888ccadf3f1599570026c57d1b6e047175c127
xxninjabunnyxx <xxninjabunnyxx@gmail.com>
2022-07-25 13:51:53 -0400

    update

---
+++ lib/skittles/terminal.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "terminal/version"
+
+module Skittles
+  module Terminal
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/skittles/terminal/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Skittles
+  module Terminal
+    VERSION = "0.1.0"
+  end
+end
